13c731a62f55e96c3b5f4641d5efb79d9f529ba6
Germanaz0 <germanazo@gmail.com>
2012-07-09 19:26:58 -0300

    Add some basics features

---
+++ nodejs/server.js
@@ -0,0 +1,21 @@
+var server = require("socket.io").listen(8080);
+
+server.sockets.on("connection", function(socket)
+{
+
+    socket.on("newMessage", function(data){
+                console.log(data);
+
+        server.sockets.emit("sendEvent", data);
+    });
+
+
+    socket.on('addUser', function(data) {
+        
+    });
+
+    socket.on('disconnect', function(data){
+        console.log('Server has disconnected');
+    });
+
+});
